1ee1a385386692fec9d7868889fd3b4f7f0b7800
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-11-29 06:45:03 +0200

    db: seed the database with admin user

--- src/main/java/dev/kiki/donation/Application.java
+++ src/main/java/dev/kiki/donation/Application.java
@@ -1,13 +1,38 @@
 package dev.kiki.donation;
 
+import dev.kiki.donation.user.Role;
+import dev.kiki.donation.user.User;
+import dev.kiki.donation.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+            User user = new User();
+            user.setFirstName("Derrick");
+            user.setLastName("Nuby");
+            user.setRole(Role.ROLE_ADMIN);
+            user.setEmail("admin@gmail.com");
+            user.setPhoneNumber("+250788530944");
+            user.setUserName("DerrickNuby");
+            user.setPassword(passwordEncoder.encode("admin123@#"));
+
+            userRepository.save(user);
+        };
     }
 
 }
