5f49d0e5f2cd3c1b76e561a10965122eb3a49749
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-11-29 06:41:57 +0200

    fix: now returning singleton list of role

--- src/main/java/dev/kiki/donation/user/User.java
+++ src/main/java/dev/kiki/donation/user/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -54,7 +55,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.singletonList(role);
     }
 
     @Override
